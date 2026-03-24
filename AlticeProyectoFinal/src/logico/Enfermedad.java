6550cec378e75262e87610b5c077361bc6026c01
misar <misar@Claudio>
2026-03-24 14:43:33 -0400

    Preparando proyecto

--- AlticeProyectoFinal/src/logico/Enfermedad.java
+++
@@ -1,6 +0,0 @@
-package logico;
-
-public class Enfermedad {
-	private String id;
-	private String nombre;
-}
--- AlticeProyectoFinal/src/logico/Vacuna.java
+++
@@ -1,8 +0,0 @@
-package logico;
-
-public class Vacuna {
-	public String id;
-	public String descripcion;
-	public String nombre;
-
-}
