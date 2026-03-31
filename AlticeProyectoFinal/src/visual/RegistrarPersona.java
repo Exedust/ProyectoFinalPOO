78db5177304f54b91b0b6d9baa2703cc51768ae2
misar <misar@Claudio>
2026-03-31 10:50:38 -0400

    Quitando pantallas de registrar cliente y empleado por RegistrarPersona

--- AlticeProyectoFinal/src/visual/RegistrarPersona.java
+++ AlticeProyectoFinal/src/visual/RegistrarPersona.java
@@ -8,7 +8,7 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class RegistrarCliente extends JDialog {
+public class RegistrarPersona extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 
@@ -17,7 +17,7 @@ public class RegistrarCliente extends JDialog {
 	 */
 	public static void main(String[] args) {
 		try {
-			RegistrarCliente dialog = new RegistrarCliente();
+			RegistrarPersona dialog = new RegistrarPersona();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -28,7 +28,7 @@ public class RegistrarCliente extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public RegistrarCliente() {
+	public RegistrarPersona() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setLayout(new FlowLayout());
